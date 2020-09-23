3670f3d87287754df6dcc385ada9a26aa7f5c1b8
Jocsã Kesley <jocsadm@gmail.com>
2020-09-23 08:44:20 -0300

    Alterações na opção voltar

--- tarefas.py
+++ tarefas.py
@@ -6,43 +6,48 @@ class ListaTarefas():
     def adicionar(self, *args):
         tarefa = input("Digite uma nova tarefa: ")
         self.tarefas.append(tarefa)
+        self.acoes.clear()
         self.acoes.update({'adicionar': tarefa})
         self.listar()
         return self.tarefas
     def remover(self, *args):
         tarefa = int(input("Digite o numero da tarefa que deseja remover :"))
         self.tarefas.pop(tarefa)
+        self.acoes.clear()
         self.acoes.update({'remover': tarefa})
+        self.listar()
         return self.tarefas
     def listar(self):
         for i, n in enumerate(self.tarefas):
             print(f"{i+1} - {n}")
     def voltar(self):
+        print(self.acoes)
         if self.acoes['adicionar']:
-            return self.adicionar(self.acoes.values())
+            self.listar()
+            return self.tarefas.remove(self.acoes['adicionar'])
         if self.acoes['remover']:
-            return self.remover(self.acoes.values())
-
+            self.listar()
+            return self.tarefas.append(self.acoes['remover'])
 lista1 = ListaTarefas()
 
 while True:
-    menu = int(input('''
+    menu = input('''
 Digite a ação desejada:
 1 - Adicionar
 2 - Remover
 3 - Listar
 4 - Voltar
 5 - Refazer
-'''))
-    if menu == 1:
+''')
+    if int(menu) == 1:
         lista1.adicionar()
-    elif menu == 2:
+    elif int(menu) == 2:
         lista1.remover()
-    elif menu == 3:
+    elif int(menu) == 3:
         lista1.listar()
-    elif menu == 4:
+    elif int(menu) == 4:
         lista1.voltar()
-    elif menu == 5:
+    elif int(menu) == 5:
         lista1.refazer()
     else:
         print("Opção inválida")
